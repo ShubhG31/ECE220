3dff358a2caa9e956620d305a74a05bcaa597847
shubhg3 <shubhg3@illinois.edu>
2021-03-26 20:28:42 -0500

    Update sudoku.c

--- mp7/sudoku.c
+++ mp7/sudoku.c
@@ -130,7 +130,7 @@ int i, j, singleIndex;
   }
 
 for (int num =1; num<=9; num++){
-  if(is_val_valid(num, i, j, sudoku)==1){
+  if(is_val_valid(num, i, j, sudoku)){
     sudoku[i][j]=num;
     if(solve_sudoku(sudoku)){
       return 1;
